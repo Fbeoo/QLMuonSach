c89b2ad6815eb134bee82d714ed8cc174d1cd3f7
Phong <hongphongle774@gmail.com>
2024-01-15 18:44:34 +0700

    create migration table category

--- database/migrations/2024_01_15_031335_create_category_table.php
+++ database/migrations/2024_01_15_031335_create_category_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('category', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('category_name');
+            $table->integer('category_parent_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('category');
     }
 };
