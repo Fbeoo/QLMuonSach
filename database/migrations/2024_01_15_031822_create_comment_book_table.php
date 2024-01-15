03bf3784e77f96e08456b444f25b9ad03d84ed8d
Phong <hongphongle774@gmail.com>
2024-01-15 18:46:04 +0700

    create migration table comment_book

---
+++ database/migrations/2024_01_15_031822_create_comment_book_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comment_book', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('book_id')->references('id')->on('book');
+            $table->foreign('user_id')->references('id')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comment_book');
+    }
+};
