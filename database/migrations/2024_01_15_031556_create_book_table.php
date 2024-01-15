f0967e124d4d81c5e025db521bfed71627f2efc0
Phong <hongphongle774@gmail.com>
2024-01-15 18:45:12 +0700

    create migration table book

---
+++ database/migrations/2024_01_15_031556_create_book_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name');
+            $table->integer('year_publish');
+            $table->integer('price_rent');
+            $table->integer('weight');
+            $table->integer('total_page');
+            $table->string('thumbnail');
+            $table->binary('status');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book');
+    }
+};
