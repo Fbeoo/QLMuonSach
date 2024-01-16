4c69d2aaff4d866916e98d00fab50fe85cadecd4
Phong <hongphongle774@gmail.com>
2024-01-16 14:27:01 +0700

    add ReportCompensation Repository

---
+++ app/Repositories/Eloquent/ReportCompensationRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\ReportCompensationRepositoryInterface;
+
+class ReportCompensationRepository extends BaseRepository implements ReportCompensationRepositoryInterface {
+
+}
