d3f560cbe9d42dc1b5d775dc511c4c498394863f
Phong <hongphongle774@gmail.com>
2024-01-16 14:23:31 +0700

    add AuthorInfo Repository

---
+++ app/Repositories/Eloquent/AuthorInfoRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\AuthorInfoRepositoryInterface;
+
+class AuthorInfoRepository extends BaseRepository implements AuthorInfoRepositoryInterface {
+
+}
