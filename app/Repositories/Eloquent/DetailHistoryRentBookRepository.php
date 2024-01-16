782293854ac882a62ee6734a26bd6fb5190b7881
Phong <hongphongle774@gmail.com>
2024-01-16 14:26:05 +0700

    add DetailHistoryRentBook Repository

---
+++ app/Repositories/Eloquent/DetailHistoryRentBookRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\DetailHistoryRentBookRepositoryInterface;
+
+class DetailHistoryRentBookRepository extends BaseRepository implements DetailHistoryRentBookRepositoryInterface {
+
+}
