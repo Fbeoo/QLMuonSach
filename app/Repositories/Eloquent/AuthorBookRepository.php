d20ef7581ff42f18991c8ea5abeb6a19361da931
Phong <hongphongle774@gmail.com>
2024-01-16 14:22:55 +0700

    add AuthorBook Repository

---
+++ app/Repositories/Eloquent/AuthorBookRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\AuthorBookRepositoryInterface;
+
+class AuthorBookRepository extends BaseRepository implements AuthorBookRepositoryInterface {
+
+}
