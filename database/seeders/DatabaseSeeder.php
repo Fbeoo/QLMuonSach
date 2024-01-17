49a819682916c61439d097454b5f32931d7fbc45
Phong <hongphongle774@gmail.com>
2024-01-17 14:58:25 +0700

    call seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\CommentBook;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CategorySeeder::class);
+        $this->call([
+            CategorySeeder::class,
+            BookSeeder::class,
+            AuthorInfoSeeder::class,
+            AuthorBookSeeder::class,
+            UserSeeder::class,
+            HistoryRentBookSeeder::class,
+            DetailHistoryRentBookSeeder::class,
+            AdminSeeder::class,
+            CommentBookSeeder::class
+        ]);
     }
 }
