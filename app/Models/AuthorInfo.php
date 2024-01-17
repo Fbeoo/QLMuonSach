0250c5779376a11f51be35720c96ff23c187e607
Phong <hongphongle774@gmail.com>
2024-01-17 11:27:17 +0700

    add softdelete and $table

--- app/Models/AuthorInfo.php
+++ app/Models/AuthorInfo.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AuthorInfo extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $table = 'author_info';
     protected $fillable = [
         'author_name',
         'dob',
--- app/Models/ReportCompensation.php
+++ app/Models/ReportCompensation.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReportCompensation extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $table = 'report_compensation';
     protected $fillable = [
         'content',
         'quantity',
