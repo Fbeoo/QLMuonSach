f3258a2ed542557f624dca2244375c1d7e70feee
Phong <hongphongle774@gmail.com>
2024-01-18 17:19:04 +0700

    fix model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
     protected $table = 'user';
+    protected $guarded = 'web';
     protected $fillable = [
         'name',
         'dob',
