fa4e28609692dac508f295bf56b6651a115b8278
Phong <hongphongle774@gmail.com>
2024-01-18 17:17:05 +0700

    add get all book for all page

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\BookRepositoryInterface;
+use App\Repositories\Eloquent\BookRepository;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    protected $bookRepository;
+
+    /**
+     * @param $bookRepository
+     */
+    public function __construct(BookRepositoryInterface $bookRepository)
+    {
+        $this->bookRepository = $bookRepository;
+    }
+    public function getBookForHomePage() {
+        return view('index',['books'=>$this->bookRepository->getALl()]);
+    }
 }
