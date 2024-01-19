1ab3d2d6970d48a5c583048852d51f998349f78a
Phong <hongphongle774@gmail.com>
2024-01-19 11:45:42 +0700

    fix view book in home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,27 @@
 @include('layout.header')
 <x-sidebar/>
-<div class="col-12">
+<h1>Home</h1>
+<div class="col-10" style="margin: auto">
     <div class="card card-primary">
         <div class="card-body">
             <div>
                 <div class="filter-container p-0 row">
                     @foreach($books as $book)
-                        <div class="filtr-item col-sm-2" style="border: 1px solid rgb(233, 233, 233); text-align: center">
-                            <div class="book_image">
-                                <img src="{{asset('dist/img/'.$book->thumbnail)}}" style="max-width: 60%">
+                        <div class="filtr-item col-sm-3" style="border: 1px solid rgb(233, 233, 233); text-align: center;">
+                            <div style="padding: 30px">
+                                <div class="book_image">
+                                    <img src="{{asset('dist/img/'.$book->thumbnail)}}" style="max-width: 60%; padding: 20px">
+                                </div>
+                                <div class="book_info" style="height: 80px">
+                                    <h6 class="book_name"><a>{{$book->name}}</a></h6>
+                                    <div class="rent_price" style="font-size: 16px;color: #fe4c50;font-weight: 600">{{$book->price_rent}}VNĐ /1 tháng</div>
+                                </div>
+                                <button type="button" class="btn btn-block btn-primary">Chi tiết</button>
                             </div>
-                            <div class="book_info" style="">
-                                <h6 class="book_name"><a>{{$book->name}}</a></h6>
-                                <div class="rent_price" style="font-size: 16px;color: #fe4c50;font-weight: 600">{{$book->price_rent}}VNĐ /1 tháng</div>
                             </div>
-                            <button type="button" class="btn btn-block btn-primary">Chi tiết</button>
-                        </div>
                     @endforeach
                 </div>
             </div>
-
         </div>
     </div>
 </div>
