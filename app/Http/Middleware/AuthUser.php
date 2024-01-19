a9ef86447b053db584a49c471e7f95d9d751a360
Phong <hongphongle774@gmail.com>
2024-01-19 11:47:41 +0700

    add middleware auth user

---
+++ app/Http/Middleware/AuthUser.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $credentials = $request->only('mail','password');
+        if (Auth::guard('web')->attempt($credentials)) {
+            $request->session()->regenerate();
+            $request->session()->put('user',Auth::user());
+            return redirect()->route('home');
+        }
+        else {
+            return back()->withErrors([
+                'mail' => 'Email or Password was incorrect.',
+            ])->onlyInput('mail');
+        }
+    }
+}
