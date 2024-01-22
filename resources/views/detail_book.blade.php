47355270f539b1a2e9a6eace0fb6c8d029cd8fc7
Phong <hongphongle774@gmail.com>
2024-01-22 16:52:37 +0700

    show information book into view detail book

--- resources/views/detail_book.blade.php
+++ resources/views/detail_book.blade.php
@@ -6,33 +6,37 @@
             <div class="col-12 col-sm-4">
                 <h3 class="d-inline-block d-sm-none">LOWA Men’s Renegade GTX Mid Hiking Boots Review</h3>
                 <div class="col-12">
-                    <img src="{{asset('dist/img/sach.jpg')}}" class="book-image" alt="Book Image" style="width: 60%;margin-left: 80px">
+                    <img src="{{asset('dist/img/'.$book->thumbnail)}}" class="book-image" alt="Book Image" style="width: 60%;margin-left: 80px">
                 </div>
             </div>
             <div class="col-12 col-sm-6">
-                <h3 class="my-3">LOWA Men’s Renegade GTX Mid Hiking Boots Review</h3>
-                <p>Tác giả : Fbeo</p>
-                <p>Năm xuất bản : 1999</p>
-                <p>Trọng lượng : 500g</p>
-                <p>Số trang : 299</p>
+                <h3 class="my-3">{{$book->name}}</h3>
+                <p>Tác giả : |
+                    @foreach($authors as $author)
+                        {{$author->author_name}} |
+                    @endforeach
+                </p>
+                <p>Năm xuất bản : {{$book->year_publish}}</p>
+                <p>Trọng lượng : {{$book->weight}}g</p>
+                <p>Số trang : {{$book->total_page}}</p>
 
                 <hr>
 
                 <div class="bg-gray py-2 px-3 mt-4">
                     <h2 class="mb-0" style="color: red">
-                        20.000 VNĐ
+                        {{$book->price_rent}} VNĐ / 1 tháng
                     </h2>
                 </div>
 
                 <div class="mt-4">
                     <div class="btn btn-primary btn-lg btn-flat">
                         <i class="fas fa-cart-plus fa-lg mr-2"></i>
-                        Add to Cart
+                        Thuê
                     </div>
 
                     <div class="btn btn-default btn-lg btn-flat">
                         <i class="fas fa-heart fa-lg mr-2"></i>
-                        Add to Wishlist
+                        Thêm vào giỏ
                     </div>
                 </div>
 
@@ -45,7 +49,7 @@
                 </div>
             </nav>
             <div class="tab-content p-3" id="nav-tabContent">
-                <div class="tab-pane fade show active"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae condimentum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed posuere, purus at efficitur hendrerit, augue elit lacinia arcu, a eleifend sem elit et nunc. Sed rutrum vestibulum est, sit amet cursus dolor fermentum vel. Suspendisse mi nibh, congue et ante et, commodo mattis lacus. Duis varius finibus purus sed venenatis. Vivamus varius metus quam, id dapibus velit mattis eu. Praesent et semper risus. Vestibulum erat erat, condimentum at elit at, bibendum placerat orci. Nullam gravida velit mauris, in pellentesque urna pellentesque viverra. Nullam non pellentesque justo, et ultricies neque. Praesent vel metus rutrum, tempus erat a, rutrum ante. Quisque interdum efficitur nunc vitae consectetur. Suspendisse venenatis, tortor non convallis interdum, urna mi molestie eros, vel tempor justo lacus ac justo. Fusce id enim a erat fringilla sollicitudin ultrices vel metus. </div>
+                <div class="tab-pane fade show active">{{$book->description}}</div>
             </div>
         </div>
     </div>
