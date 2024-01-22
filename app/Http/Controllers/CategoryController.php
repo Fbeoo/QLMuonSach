03a1e900ec13aa2ae89c577c58189a44c26fde06
Phong <hongphongle774@gmail.com>
2024-01-22 16:52:02 +0700

    add function get category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,5 +8,19 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    protected $categoryRepository;
+
+    /**
+     * @param $categoryRepository
+     */
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+    public function getCategoryParent() {
+        return $this->categoryRepository->getCategoryParent();
+    }
+    public function getCategoryChildren($categoryParentId) {
+        return $this->categoryRepository->getCategoryChildByCategoryParentId($categoryParentId);
+    }
 }
