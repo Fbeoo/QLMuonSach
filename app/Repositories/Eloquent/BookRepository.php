4e26444981d0606f8e82385c87498e7d9d74dffd
Phong <hongphongle774@gmail.com>
2024-01-22 16:51:21 +0700

    add get book by category

--- app/Repositories/Eloquent/BookRepository.php
+++ app/Repositories/Eloquent/BookRepository.php
@@ -3,5 +3,12 @@ namespace App\Repositories\Eloquent;
 use App\Repositories\BookRepositoryInterface;
 
 class BookRepository extends BaseRepository implements BookRepositoryInterface {
+    public function getBookForPaging() {
+        return $this->model->paginate(4);
+    }
 
+    public function getBookByCategory($categoryId)
+    {
+        return $this->model->where('category_id',$categoryId)->get();
+    }
 }
