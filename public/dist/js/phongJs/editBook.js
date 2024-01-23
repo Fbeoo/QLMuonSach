4575a94061d75d889702cf1c6cc278605c5e5c1f
Phong <hongphongle774@gmail.com>
2024-01-23 17:09:23 +0700

    add ajax to edit book

--- public/dist/js/phongJs/editBook.js
+++ public/dist/js/phongJs/editBook.js
@@ -14,7 +14,74 @@ function getCategoryChildren(categoryParentId) {
                 strHtml += `<option value="${response[i].id}" id="${response[i].id}">${response[i].category_name}</option>`
             }
             console.log(strHtml);
-            $('#inputCategoryChildren').html(strHtml);
+            $('#categoryChildren').html(strHtml);
+        }
+    });
+}
+
+function editBook() {
+    $.ajax({
+        url: 'http://localhost:8000/api/admin/edit/book',
+        method: 'PUT',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+            "id" : document.getElementById('id').value,
+            "name": document.getElementById('bookName').value,
+            "year_publish" : document.getElementById('yearPublish').value,
+            "price_rent" : document.getElementById('priceRent').value,
+            "weight" : document.getElementById('weight').value,
+            "total_page" : document.getElementById('totalPage').value,
+            "thumbnail" : document.getElementById('bookImage').value,
+            "category_id" : document.getElementById('categoryChildren').value,
+            "quantity" : document.getElementById('quantity').value,
+            "description" : document.getElementById('bookDescription').value
+        }),
+        error: function(xhr, textStatus, errorThrown) {
+            console.log(xhr.responseText);
+            alert('Lỗi: ' + xhr.responseText);
+        },
+        success: function () {
+            alert('Sửa sách thành công');
+            window.location.href = "http://localhost:8000/admin/manage/book";
+        }
+    });
+}
+
+function validateInputToEditBook() {
+    $.ajax({
+        url: 'http://localhost:8000/api/admin/validate',
+        method: 'POST',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+            "id" : document.getElementById('id').value,
+            "name": document.getElementById('bookName').value,
+            "year_publish" : document.getElementById('yearPublish').value,
+            "price_rent" : document.getElementById('priceRent').value,
+            "weight" : document.getElementById('weight').value,
+            "total_page" : document.getElementById('totalPage').value,
+            "thumbnail" : document.getElementById('bookImage').value,
+            "category_id" : document.getElementById('categoryChildren').value,
+            "quantity" : document.getElementById('quantity').value,
+            "description" : document.getElementById('bookDescription').value
+        }),
+        error: function(xhr, textStatus, errorThrown) {
+            console.log(xhr.responseText);
+            alert('Lỗi: ' + xhr.responseText);
+        },
+        success: function (response) {
+            var strHtml = '';
+            if (response.errors) {
+                for (var key in response.errors) {
+                    strHtml = `<p style="color: red">${response.errors[key]}</p>`
+                    $('#'+key).html(strHtml);
+                }
+            }
+            else {
+                console.log(this.data);
+                editBook();
+            }
         }
     });
 }
@@ -35,4 +102,7 @@ imageInput.addEventListener('change', function(event) {
 var selectCategoryParent = document.getElementById('inputCategoryParent');
 selectCategoryParent.addEventListener('change',function () {
     getCategoryChildren(selectCategoryParent.selectedOptions[0].id);
+})
+document.getElementById('editBook').addEventListener('click',function () {
+    validateInputToEditBook();
 })
