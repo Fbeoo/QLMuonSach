e0f48353fed1859f4563df56fe282dcf026ae3dc
Phong <hongphongle774@gmail.com>
2024-01-23 17:07:46 +0700

    fix view add book

--- resources/views/admin/add_book.blade.php
+++ resources/views/admin/add_book.blade.php
@@ -1,8 +1,8 @@
 @include('admin.layout.header')
 @include('admin.layout.sidebar')
-<div class="card card-primary">
+<div class="card card-primary" style="width: 80%; margin: auto; margin-top: 20px">
     <div class="card-header">
-        <h3 class="card-title">General</h3>
+        <h3 class="card-title">Thêm sách</h3>
 
         <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
@@ -11,93 +11,98 @@
         </div>
     </div>
     <div class="card-body" style="display: block;">
-        <form action="{{route('addBook')}}" method="POST">
-            @csrf
-            <input type="hidden" name="bookId">
+        <div style="display: inline-block; width: 45%;">
+            <input type="hidden" name="bookId" id="bookId">
             <div class="form-group">
-                @error('bookImage')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <label for="exampleInputFile"Book Image</label>
-                <div class="input-group">
-                    <div class="custom-file" style="display: table">
-                        <input type="hidden" name="bookImage" id="bookImage">
-                        <input type="file" id="imageInput">
-                        <img id="previewImage" src="{{asset('dist/img/'.old('bookImage'))}}" alt="Preview Image" style="width: 5%">
+                <label for="inputName">Tên sách</label>
+                <input type="text" id="bookName" class="form-control" name="bookName" value="{{ old('bookName') }}">
+                <div id="nameError">
+
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputClientCompany">Năm phát hành</label>
+                <input type="text" id="yearPublish" class="form-control" name="yearPublish">
+                <div id="yearPublishError">
+
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputProjectLeader">Giá thuê</label>
+                <input type="text" id="priceRent" class="form-control" name="priceRent" value="{{ old('priceRent') }}">
+                <div id="priceRentError">
+
+                </div>
+            </div>
+        </div>
+        <div style="display: inline-block;width: 45%;margin-left: 100px">
+            <div class="form-group">
+                <label for="inputProjectLeader">Trọng lượng</label>
+                <input type="text" id="weight" class="form-control" name="weight" value="{{ old('weight') }}">
+                <div id="weightError">
+
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputProjectLeader">Tổng số trang</label>
+                <input type="text" id="totalPage" class="form-control" name="totalPage" value="{{ old('totalPage') }}">
+                <div id="totalPageError">
+
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputProjectLeader">Số lượng</label>
+                <input type="text" id="quantity" class="form-control" name="quantity" value="{{ old('quantity') }}">
+                <div id="quantityError">
+
+                </div>
+            </div>
+        </div>
+            <div class="form-group" style="display: inline-grid">
+                <label for="inputStatus">Danh mục</label>
+                <div style="display: inline-flex">
+                    <div style="width: 300px">
+                        <select id="inputCategoryParent" class="form-control custom-select">
+                            <option selected>Chọn danh mục bất kì</option>
+                            @foreach($categoryParents as $categoryParent)
+                                <option id="{{$categoryParent->id}}">{{$categoryParent->category_name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div style="width: 300px; margin-left: 30px">
+                        <select id="categoryChildren" class="form-control custom-select" name="categoryChildren">
+
+                        </select>
                     </div>
                 </div>
-            </div>
-            <div class="form-group">
-                @error('bookName')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <label for="inputName">Book Name</label>
-                <input type="text" id="inputName" class="form-control" name="bookName" value="{{ old('bookName') }}">
-            </div>
-            <div class="form-group">
-                @error('yearPublish')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <label for="inputClientCompany">Year Publish</label>
-                <input type="text" id="inputClientCompany" class="form-control" name="yearPublish" value="{{ old('yearPublish') }}">
-            </div>
-            <div class="form-group">
-                @error('priceRent')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <label for="inputProjectLeader">Price Rent</label>
-                <input type="text" id="inputProjectLeader" class="form-control" name="priceRent" value="{{ old('priceRent') }}">
-            </div>
-            <div class="form-group">
-                @error('weight')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <label for="inputProjectLeader">Weight</label>
-                <input type="text" id="inputProjectLeader" class="form-control" name="weight" value="{{ old('weight') }}">
-            </div>
-            <div class="form-group">
-                @error('totalPage')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <label for="inputProjectLeader">Total Page</label>
-                <input type="text" id="inputProjectLeader" class="form-control" name="totalPage" value="{{ old('totalPage') }}">
-            </div>
-            <div class="form-group">
-                @error('quantity')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <label for="inputProjectLeader">Quantity</label>
-                <input type="text" id="inputProjectLeader" class="form-control" name="quantity" value="{{ old('quantity') }}">
-            </div>
-            <div class="form-group" style="display: inline-grid">
-                @error('categoryChildren')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <label for="inputStatus">Category</label>
-                <div style="display: inline-flex">
-                    <select id="inputCategoryParent" class="form-control custom-select" style="width: 500px">
-                        <option selected>Select one</option>
-                        @foreach($categoryParents as $categoryParent)
-                            <option id="{{$categoryParent->id}}">{{$categoryParent->category_name}}</option>
-                        @endforeach
-                    </select>
-                    <select id="inputCategoryChildren" class="form-control custom-select" name="categoryChildren">
+                <div id="categoryError">
 
-                    </select>
                 </div>
             </div>
             <div class="form-group">
-                @error('bookDescription')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                <label for="inputDescription">Project Description</label>
-                <textarea id="inputDescription" class="form-control" rows="4" name="bookDescription">{{ old('bookDescription') }}</textarea>
+                <label for="inputDescription">Mô tả</label>
+                <textarea id="bookDescription" class="form-control" rows="4" name="bookDescription">{{ old('bookDescription') }}</textarea>
+                <div id="descriptionError">
+
+                </div>
             </div>
-            <input type="submit" value="Add">
-        </form>
+        <div class="form-group">
+            <label for="exampleInputFile">Ảnh</label>
+            <div class="input-group">
+                <div class="custom-file" style="display: table">
+                    <input type="hidden" name="bookImage" id="bookImage">
+                    <input type="file" id="imageInput">
+                    <img id="previewImage" src="{{asset('dist/img/'.old('bookImage'))}}" alt="Ảnh sách" style="width: 5%">
+                </div>
+            </div>
+            <div id="thumbnailError">
+
+            </div>
+        </div>
+            <a type="button" class="btn btn-block btn-success" id="addBook">Thêm mới</a>
     </div>
     <!-- /.card-body -->
 </div>
 @include('admin.layout.footer')
-<script src="{{asset('dist/js/phongJs/editBook.js')}}"></script>
+<script src="{{asset('dist/js/phongJs/addBook.js')}}"></script>
 
