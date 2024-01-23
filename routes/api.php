3fdbf18f68b99dd7fc7f482843e1e635a5818d77
Phong <hongphongle774@gmail.com>
2024-01-23 17:08:11 +0700

    add api route for edit, add , lock and unlock book

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +23,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //GET
 Route::get('/category/parent',[CategoryController::class,'getCategoryParent']);
 Route::get('/category/children/{categoryParentId}',[CategoryController::class,'getCategoryChildren']);
+
+//POST
+Route::post('/admin/add/book',[BookController::class,'addBook'])->name('addBook');
+
+Route::post('/admin/validate',[BookController::class,'validateInput']);
+
+//PUT
+Route::put('/admin/edit/book',[BookController::class,'editBook'])->name('editBook');
+
+Route::put('/admin/lock/book',[BookController::class,'lockBook'])->name('lockBook');
+
+Route::put('/admin/unlock/book',[BookController::class,'unlockBook'])->name('unlockBook');
+
+
