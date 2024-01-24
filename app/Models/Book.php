1c5c76e1041277e249476e7907394e8b687875bd
Phong <hongphongle774@gmail.com>
2024-01-24 17:12:19 +0700

    add const status book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,10 +6,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ *
+ */
 class Book extends Model
 {
     use HasFactory,SoftDeletes;
+
+    const statusAvailable = 1;
+    const statusLock = 0;
+
+    /**
+     * @var string
+     */
     protected $table = 'book';
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'name',
         'year_publish',
@@ -22,18 +35,38 @@ class Book extends Model
         'description',
         'category_id',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function category() {
         return $this->belongsTo(Category::class,'category_id');
     }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function authorInfo() {
         return $this->belongsToMany(AuthorInfo::class);
     }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function historyRentBook() {
         return $this->belongsToMany(HistoryRentBook::class);
     }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function commentingByUser() {
         return $this->belongsToMany(User::class,'comment_book');
     }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function compensatingByUser() {
         return $this->belongsToMany(User::class,'report_compensation');
     }
