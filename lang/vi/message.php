ed5e959119934a93f06a4053d713e18f048a47db
Phong <hongphongle774@gmail.com>
2024-01-24 17:14:23 +0700

    add file lang

---
+++ lang/vi/message.php
@@ -0,0 +1,35 @@
+<?php
+return [
+    'yearPublishValidateRequired' => 'Năm phát hành không được để trống',
+    'yearPublishValidateInteger' => 'Năm phát hành phải là số nguyên',
+    'yearPublishValidateMin' => 'Năm phát hành phải lớn hơn 0',
+
+    'priceRentValidateRequired' => 'Giá thuê không được để trống',
+    'priceRentValidateInteger' => 'Giá thuê phải là số nguyên',
+    'priceRentValidateMin' => 'Giá thuê phải lớn hơn 0',
+
+    'weightValidateRequired' => 'Trọng lượng không được để trống',
+    'weightValidateInteger' => 'Trọng lượng phải là số nguyên',
+    'weightValidateMin' => 'Trọng lượng phải lớn hơn 0',
+
+    'totalPageValidateRequired' => 'Tổng số trang không được để trống',
+    'totalPageValidateInterger' => 'Tổng số trang phải là số nguyên',
+    'totalPageValidateMin' => 'Tổng số trang phải lớn hơn 0',
+
+    'quantityValidateRequired' => 'Số lượng không được để trống',
+    'quantityValidateInteger' => 'Số lượng phải là số nguyên',
+    'quantityValidateMin' => 'Số lượng phải lớn hơn 0',
+
+    'categoryIdValidateRequired' => 'Danh mục không được để trống',
+
+    'thumbnailValidateRequired' => 'Ảnh của sách không được để trống',
+
+    'descriptionValidateRequired' => 'Mô tả sách không được để trống',
+
+    'nameValidateRequired' => 'Tên của sách không được để trống',
+
+    'authorIdValidateRequired' => 'Tác giả không được để trống',
+
+    'authorNameValidateRequired' => 'Tên tác giả không được để trống',
+    'authorNameValidateAlpha' => 'Tên tác giả phải là chữ'
+];
