69195da1b940056584ae0a7b9c8ec414d9735495
Phong <hongphongle774@gmail.com>
2024-01-24 17:00:22 +0700

    fix view add book and add button to add author

--- resources/views/admin/add_book.blade.php
+++ resources/views/admin/add_book.blade.php
@@ -1,5 +1,45 @@
 @include('admin.layout.header')
 @include('admin.layout.sidebar')
+<style>
+    label{
+        color:#000;
+        font-weight:500;
+        background:#fff;
+        margin-bottom:5px;
+        padding:4px;
+        border-radius:4px;
+    }
+    input{
+        margin-bottom:10px
+    }
+    .sub-bg {
+        background: url('https://the-french.co.uk/wp-content/uploads/2018/02/111-400x600.jpg')no-repeat;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .subs-header{
+        padding-top: 0px;
+        border-bottom: 0px;
+    }
+    .heading-text h4{
+        color: #fff;
+        padding: 20px;
+        border: 2px solid #d3d3d3;
+        display: inline-block;
+        border-radius: 20px;
+        box-shadow: inset 10px 12px 13px -7px rgba(0,0,0,0.7);
+        -moz-box-shadow: inset 10px 12px 13px -7px rgba(0,0,0,0.7);
+        -webkit-box-shadow: inset 10px 12px 13px -7px rgba(0,0,0,0.7);
+        -o-box-shadow: inset 10px 12px 13px -7px rgba(0,0,0,0.7);
+    }
+    .heading-text{
+        margin-top: 10px;
+    }
+    .close:not(:disabled):not(.disabled):focus, .close:not(:disabled):not(.disabled):hover {
+        outline: none;
+    }
+</style>
 <div class="card card-primary" style="width: 80%; margin: auto; margin-top: 20px">
     <div class="card-header">
         <h3 class="card-title">Thêm sách</h3>
@@ -10,6 +50,7 @@
             </button>
         </div>
     </div>
+    <form>
     <div class="card-body" style="display: block;">
         <div style="display: inline-block; width: 45%;">
             <input type="hidden" name="bookId" id="bookId">
@@ -58,10 +99,11 @@
                 </div>
             </div>
         </div>
+        <div style="width: 45%; display: inline-block">
             <div class="form-group" style="display: inline-grid">
                 <label for="inputStatus">Danh mục</label>
                 <div style="display: inline-flex">
-                    <div style="width: 300px">
+                    <div style="width: 271px">
                         <select id="inputCategoryParent" class="form-control custom-select">
                             <option selected>Chọn danh mục bất kì</option>
                             @foreach($categoryParents as $categoryParent)
@@ -69,16 +111,38 @@
                             @endforeach
                         </select>
                     </div>
-                    <div style="width: 300px; margin-left: 30px">
+                    <div style="width: 271px; margin-left: 30px">
                         <select id="categoryChildren" class="form-control custom-select" name="categoryChildren">
 
                         </select>
                     </div>
                 </div>
-                <div id="categoryError">
+                <div id="categoryIdError">
 
                 </div>
             </div>
+        </div>
+        <div style="width: 45%; display: inline-block; margin-left: 100px">
+            <div class="form-group" style="display: inline-grid;">
+                <label for="inputStatus">Tác giả</label>
+                <div style="display: inline-flex">
+                    <div style="width: 300px">
+                        <select id="author" class="form-control custom-select">
+                            <option value="">Chọn tác giả bất kì</option>
+                            @foreach($authors as $author)
+                                <option value="{{$author->id}}" id="{{$author->id}}">{{$author->author_name}}</option>
+                            @endforeach
+                        </select>
+                        <div id="authorIdError">
+
+                        </div>
+                    </div>
+                    <div style="margin-left: 30px">
+                        <a class="btn btn-primary" data-toggle="modal" href='#susbc-form'>+</a>
+                    </div>
+                </div>
+            </div>
+        </div>
             <div class="form-group">
                 <label for="inputDescription">Mô tả</label>
                 <textarea id="bookDescription" class="form-control" rows="4" name="bookDescription">{{ old('bookDescription') }}</textarea>
@@ -103,6 +167,40 @@
     </div>
     <!-- /.card-body -->
 </div>
+<div class="modal fade" id="susbc-form">
+    <div class="modal-dialog shadow-lg p-3 mb-5 bg-white rounded">
+        <div class="modal-content sub-bg">
+            <div class="modal-header subs-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <!-- <h4 class="modal-title">Modal title</h4> -->
+            </div>
+            <div class="modal-body">
+                <div class="text-center">
+                    <h4>Thêm tác giả</h4>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <form id="subs-form">
+                            <div class="form-group row">
+                                <div class="col-md-12 col-xs-12">
+                                    <label for="firstName">Tên tác giả</label>
+                                    <input type="text" class="form-control" id="authorName">
+                                    <div id="authorNameError">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <a id="addAuthor" class="btn btn-primary text-center">Thêm mới</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 @include('admin.layout.footer')
 <script src="{{asset('dist/js/phongJs/addBook.js')}}"></script>
 
