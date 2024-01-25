e73c1966a133ae0f64cdc43a76d436d5b7bcd367
Phong <hongphongle774@gmail.com>
2024-01-25 17:04:10 +0700

    add input search book

--- resources/views/admin/manage_book.blade.php
+++ resources/views/admin/manage_book.blade.php
@@ -1,18 +1,42 @@
 @include('admin.layout.header')
 @include('admin.layout.sidebar')
 <a type="button" class="btn btn-block btn-success" style="margin-left: auto; width: 10%" href="{{route('addBookPage')}}">Thêm mới</a>
+<div style="margin-left: 20px">
+    <div class="form-group" style="display: inline-grid">
+{{--            <div style="display: inline-flex">--}}
+{{--                <div style="width: 271px">--}}
+{{--                    <label>Sắp xếp theo giá thuê</label>--}}
+{{--                    <select id="1" class="form-control custom-select">--}}
+{{--                        <option selected>Tăng dần</option>--}}
+{{--                        <option selected>Giảm dần</option>--}}
+{{--                    </select>--}}
+{{--                </div>--}}
+{{--                <div style="width: 271px; margin-left: 30px">--}}
+
+{{--                </div>--}}
+{{--            </div>--}}
+        <div class="input-group" style="margin-top: 20px">
+            <input id="bookSearch" type="search" class="form-control form-control-lg" placeholder="Nhập tên sách để tìm kiếm" style="height: 40px; width: 500px">
+            <div class="input-group-append" style="height: 40px">
+                <a class="btn btn-default" id="search">
+                    <i class="fa fa-search"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="card-body">
     <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6"></div><div class="col-sm-12 col-md-6"></div></div><div class="row"><div class="col-sm-12"><table id="example2" class="table table-bordered table-hover dataTable dtr-inline" aria-describedby="example2_info">
                     <thead>
                     <tr><th class="sorting sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Id</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Tên sách</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Hình ảnh</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Năm phát hành</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">giá thuê</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Trọng lượng</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Tổng số trang</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Số lượng</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Trạng thái</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Hành động</th></tr>
                     </thead>
-                    <tbody>
+                    <tbody id="showBooks">
                         @foreach($books as $book)
                             <tr class="odd">
                             <td class="dtr-control sorting_1" tabindex="0">{{$book->id}}</td>
                             <td>{{$book->name}}</td>
                             <td style="width: 100px; height: 100px">
-                                <img src="{{asset('dist/img/'.$book->thumbnail)}}" style="max-width: 100%">
+                                <img src="{{asset('storage/'.$book->thumbnail)}}" style="max-width: 100%">
                             </td>
                             <td>{{$book->year_publish}}</td>
                             <td>{{$book->price_rent}}</td>
@@ -48,5 +72,7 @@
                 </table>
     </div>
             <script src="{{asset('dist/js/phongJs/lockBook.js')}}"></script>
+            <script src="{{asset('dist/js/phongJs/manageBook.js')}}"></script>
 @include('admin.layout.footer')
+
 
