d91d26b6e0f00c7ab5e76063b343c482f57682e2
Phong <hongphongle774@gmail.com>
2024-01-29 17:16:14 +0700

    add paging for book and add filter by category, status, year publish

--- resources/views/admin/manage_book.blade.php
+++ resources/views/admin/manage_book.blade.php
@@ -1,34 +1,108 @@
 @include('admin.layout.header')
 @include('admin.layout.sidebar')
-<a type="button" class="btn btn-block btn-success" style="margin-left: auto; width: 10%" href="{{route('addBookPage')}}">Thêm mới</a>
-<div style="margin-left: 20px">
-    <div class="form-group" style="display: inline-grid">
-{{--            <div style="display: inline-flex">--}}
-{{--                <div style="width: 271px">--}}
-{{--                    <label>Sắp xếp theo giá thuê</label>--}}
-{{--                    <select id="1" class="form-control custom-select">--}}
-{{--                        <option selected>Tăng dần</option>--}}
-{{--                        <option selected>Giảm dần</option>--}}
-{{--                    </select>--}}
-{{--                </div>--}}
-{{--                <div style="width: 271px; margin-left: 30px">--}}
+<style>
+    .pagination {
+        display: flex;
+        margin-top: 20px;
+    }
 
-{{--                </div>--}}
-{{--            </div>--}}
-        <div class="input-group" style="margin-top: 20px">
-            <input id="bookSearch" type="search" class="form-control form-control-lg" placeholder="Nhập tên sách để tìm kiếm" style="height: 40px; width: 500px">
-            <div class="input-group-append" style="height: 40px">
-                <a class="btn btn-default" id="search">
-                    <i class="fa fa-search"></i>
-                </a>
-            </div>
+    .pagination ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .pagination li {
+        display: inline-block;
+        margin-right: 5px;
+    }
+
+    .pagination li a {
+        display: block;
+        padding: 8px 12px;
+        text-decoration: none;
+        background-color: #f2f2f2;
+        color: #333;
+    }
+
+    .pagination li a:hover {
+        background-color: #ddd;
+    }
+
+    .pagination li a.active {
+        background-color: #4CAF50;
+        color: white;
+    }
+</style>
+<a type="button" class="btn btn-block btn-success" style="margin-left: auto; width: 10%; margin-top: 20px" href="{{route('addBookPage')}}">Thêm mới</a>
+<div style="display: flex; justify-content: center; align-items: center;">
+<div style="margin-left: 20px">
+    <div class="input-group" style="margin-top: 20px">
+        <input id="bookSearch" type="search" class="form-control form-control-lg" placeholder="Nhập tên sách để tìm kiếm" style="height: 40px; width: 500px">
+        <div class="input-group-append" style="height: 40px">
+            <button class="btn btn-default" id="search">
+                <i class="fa fa-search"></i>
+            </button>
         </div>
     </div>
+    <div class="form-group" style="display: inline-grid; margin-top: 20px">
+            <div style="display: inline-flex">
+                <div style="width: 271px">
+                    <label>Năm phát hành</label>
+                    <select id="sortBookByYearPublish" class="form-control custom-select">
+                        <option selected>Sắp xếp theo năm phát hành</option>
+                        <option value="asc">Tăng dần</option>
+                        <option value="desc">Giảm dần</option>
+                    </select>
+                    <div style="display: inline-flex; margin-top: 20px">
+                        <div>
+                            <input class="form-control" type="text" style="width: 60px" id="minYear">
+                            <p id="minYearError">
+
+                            </p>
+                        </div>
+                        <div>
+                            <input class="form-control" type="text" style="width: 60px; margin-left: 30px" id="maxYear">
+                            <p id="maxYearError">
+
+                            </p>
+                        </div>
+                        <div class="input-group-append" style="height: 40px; margin-left: 30px">
+                            <button class="btn btn-default" id="searchByYear">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div style="width: 271px; margin-left: 30px">
+                    <label>Trạng thái</label>
+                    <select id="filterBookByStatus" class="form-control custom-select">
+                        <option selected>Sắp xếp theo trạng thái</option>
+                        <option value="available">Bình thường</option>
+                        <option value="lock">Khóa</option>
+                    </select>
+                </div>
+                <div style="width: 271px; margin-left: 30px">
+                    <label>Danh mục</label>
+                    <select id="filterBookByCategoryParent" class="form-control custom-select">
+                        <option selected>Sắp xếp theo danh mục lớn</option>
+                        @foreach($categories as $category)
+                            <option value="{{$category->id}}">{{$category->category_name}}</option>
+                        @endforeach
+                    </select>
+                    <select id="filterBookByCategoryChildren" class="form-control custom-select" style="margin-top: 20px">
+
+                    </select>
+                </div>
+            </div>
+
+    </div>
+</div>
 </div>
 <div class="card-body">
     <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6"></div><div class="col-sm-12 col-md-6"></div></div><div class="row"><div class="col-sm-12"><table id="example2" class="table table-bordered table-hover dataTable dtr-inline" aria-describedby="example2_info">
                     <thead>
-                    <tr><th class="sorting sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Id</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Tên sách</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Hình ảnh</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Năm phát hành</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">giá thuê</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Trọng lượng</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Tổng số trang</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Số lượng</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Trạng thái</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Hành động</th></tr>
+                    <tr><th class="sorting sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Id</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Tên sách</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Hình ảnh</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Năm phát hành</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">Giá thuê</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Danh mục</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Tác giả</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Số lượng</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Trạng thái</th><th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">Hành động</th></tr>
                     </thead>
                     <tbody id="showBooks">
                         @foreach($books as $book)
@@ -39,9 +113,15 @@
                                 <img src="{{asset('storage/'.$book->thumbnail)}}" style="max-width: 100%">
                             </td>
                             <td>{{$book->year_publish}}</td>
-                            <td>{{$book->price_rent}}</td>
-                            <td>{{$book->weight}}</td>
-                            <td>{{$book->total_page}}</td>
+                            <td>{{number_format($book->price_rent, 0, ',', '.')}}</td>
+                            <td>
+                                {{$book->category->category_name}}
+                            </td>
+                            <td>
+                                @foreach ($book->authorBook as $author_book)
+                                    {{$author_book->authorInfo->author_name}}
+                                @endforeach
+                            </td>
                             <td>{{$book->quantity}}</td>
                             @if($book->status === 1)
                                 <td style="color: green" id="statusBook{{$book->id}}">Bình thường</td>
@@ -58,9 +138,9 @@
                                     <div class="dropdown-menu" role="menu" style="">
                                         <a class="dropdown-item" href="{{route('editBookPage',['bookId'=>$book->id])}}">Sửa</a>
                                         @if($book->status === 1)
-                                            <a style="cursor: pointer" class="dropdown-item" id="actionStatusBook{{$book->id}}" onclick="lockBook({{$book->id}})">Khóa</a>
+                                            <button style="cursor: pointer" class="dropdown-item" id="actionStatusBook{{$book->id}}" onclick="lockBook({{$book->id}})">Khóa</button>
                                         @else
-                                            <a style="cursor: pointer" class="dropdown-item" id="actionStatusBook{{$book->id}}" onclick="unlockBook({{$book->id}})">Mở khóa</a>
+                                            <button style="cursor: pointer" class="dropdown-item" id="actionStatusBook{{$book->id}}" onclick="unlockBook({{$book->id}})">Mở khóa</button>
                                         @endif
 
                                     </div>
@@ -70,6 +150,15 @@
                         @endforeach
                     </tbody>
                 </table>
+                <div class="pagination">
+                    <ul id="ulPagination">
+                        @for($i = 1; $i <= $books->lastPage(); $i++)
+                            <li>
+                                <a href="{{ route('manageBook', ['page' => $i]) }}">{{ $i }}</a>
+                            </li>
+                        @endfor
+                    </ul>
+                </div>
     </div>
             <script src="{{asset('dist/js/phongJs/lockBook.js')}}"></script>
             <script src="{{asset('dist/js/phongJs/manageBook.js')}}"></script>
