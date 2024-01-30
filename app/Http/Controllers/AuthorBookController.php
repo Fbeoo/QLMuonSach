f5cbf1bd31b444af5b3b3f3d840a4c50eecde96f
Phong <hongphongle774@gmail.com>
2024-01-30 16:59:15 +0700

    add function show author

--- app/Http/Controllers/AuthorBookController.php
+++ app/Http/Controllers/AuthorBookController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\AuthorBookRepositoryInterface;
 use Illuminate\Http\Request;
 
 class AuthorBookController extends Controller
 {
-    //
+    protected $authorBookRepository;
+
+    /**
+     * @param $authorBookRepository
+     */
+    public function __construct(AuthorBookRepositoryInterface $authorBookRepository)
+    {
+        $this->authorBookRepository = $authorBookRepository;
+    }
+    public function showBookOfAuthor($authorId) {
+        try {
+            $authorAndBook = $this->authorBookRepository->getBookOfAuthor($authorId);
+            return view('bookOfAuthor',['authorAndBookInfo'=>$authorAndBook]);
+        }catch (\Exception $e) {
+            throw new \Exception($e);
+        }
+    }
 }
