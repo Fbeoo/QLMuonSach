fc61e476567d0517efa7c626301d53af0e5f3c95
Phong <hongphongle774@gmail.com>
2024-01-30 17:00:14 +0700

    fix add author and show all author

--- app/Http/Controllers/AuthorInfoController.php
+++ app/Http/Controllers/AuthorInfoController.php
@@ -7,6 +7,7 @@ use App\Models\AuthorInfo;
 use App\Repositories\AuthorInfoRepositoryInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 /**
  *
@@ -49,7 +50,8 @@ class AuthorInfoController extends Controller
         try {
             $authorInfo = new AuthorInfo();
             $validation = Validator::make($request->all(),[
-                'authorName' => 'required|alpha'
+                'authorName' => 'required',
+                'authorImage' => 'required|mimes:jpeg,png,jpg|max:4096'
             ],[
                 'authorName.required' => @trans('message.authorNameValidateRequired'),
                 'authorName.alpha' => @trans('message.authorNameValidateAlpha')
@@ -57,12 +59,22 @@ class AuthorInfoController extends Controller
             if ($validation->fails()) {
                 return response()->json(['errorValidate'=>$validation->errors()]);
             }
+            $path = $request->file('authorImage')->store('public/img');
             $authorInfo->author_name = $request->input('authorName');
+            $authorInfo->author_image = Str::after($path,'/');
             $this->authorInfoRepository->add($authorInfo);
             return response()->json(['success','Thêm tác giả thành công']);
         }catch (\Exception $e) {
             return response()->json(['error'=>$e]);
         }
+    }
 
+    public function showAuthorInAllAuthorPage() {
+        try {
+            $authors = $this->authorInfoRepository->getAuthorForAllAuthorPage();
+            return view('allAuthor',['authors'=>$authors]);
+        }catch (\Exception $e) {
+            throw new \Exception($e);
+        }
     }
 }
