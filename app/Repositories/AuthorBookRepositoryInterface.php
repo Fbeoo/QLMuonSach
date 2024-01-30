4274e368129eebecaa004ee06a2367094b21469f
Phong <hongphongle774@gmail.com>
2024-01-30 16:59:37 +0700

    add function show book of author

--- app/Repositories/AuthorBookRepositoryInterface.php
+++ app/Repositories/AuthorBookRepositoryInterface.php
@@ -6,4 +6,6 @@ interface AuthorBookRepositoryInterface extends EloquentRepositoryInterface {
      * @return mixed
      */
     public function getAuthorBook ($bookId);
+
+    public function getBookOfAuthor($authorId);
 }
--- app/Repositories/Eloquent/AuthorBookRepository.php
+++ app/Repositories/Eloquent/AuthorBookRepository.php
@@ -21,4 +21,14 @@ class AuthorBookRepository extends BaseRepository implements AuthorBookRepositor
             throw new \Exception($e);
         }
     }
+
+    public function getBookOfAuthor($authorId)
+    {
+        try {
+            $books = $this->model->where('author_id',$authorId)->with('book','authorInfo')->get();
+            return $books;
+        }catch (\Exception $e) {
+            throw new \Exception($e);
+        }
+    }
 }
