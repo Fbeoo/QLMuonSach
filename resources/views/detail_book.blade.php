ea5031c2a02ce76a2bd56c20bdae3bed59389739
Phong <hongphongle774@gmail.com>
2024-01-30 17:03:05 +0700

    fix variable in view detail book

--- resources/views/detail_book.blade.php
+++ resources/views/detail_book.blade.php
@@ -6,15 +6,13 @@
             <div class="col-12 col-sm-4">
                 <h3 class="d-inline-block d-sm-none">LOWA Men’s Renegade GTX Mid Hiking Boots Review</h3>
                 <div class="col-12">
-                    <img src="{{asset('dist/img/'.$book->thumbnail)}}" class="book-image" alt="Book Image" style="width: 60%;margin-left: 80px">
+                    <img src="{{asset('storage/'.$book->thumbnail)}}" class="book-image" alt="Book Image" style="width: 60%;margin-left: 80px">
                 </div>
             </div>
             <div class="col-12 col-sm-6">
                 <h3 class="my-3">{{$book->name}}</h3>
-                <p>Tác giả : |
-                    @foreach($authors as $author)
-                        {{$author->author_name}} |
-                    @endforeach
+                <p>Tác giả : {{$book->authorBook[0]->authorInfo->author_name}}
+
                 </p>
                 <p>Năm xuất bản : {{$book->year_publish}}</p>
                 <p>Trọng lượng : {{$book->weight}}g</p>
@@ -24,12 +22,12 @@
 
                 <div class="bg-gray py-2 px-3 mt-4">
                     <h2 class="mb-0" style="color: red">
-                        {{$book->price_rent}} VNĐ / 1 tháng
+                        {{number_format($book->price_rent, 0, ',', '.')}}VNĐ /1 tháng
                     </h2>
                 </div>
 
                 <div class="mt-4">
-                    <div class="btn btn-primary btn-lg btn-flat">
+                    <div class="btn btn-primary btn-lg btn-flat" data-toggle="modal" data-target="#exampleModal">
                         <i class="fas fa-cart-plus fa-lg mr-2"></i>
                         Thuê
                     </div>
@@ -55,4 +53,28 @@
     </div>
     <!-- /.card-body -->
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Thời gian thuê và số lượng</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <input type="text" class="form-control datepicker" id="datepicker" name="datepicker" placeholder="Select date">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Thoát</button>
+                <button type="button" class="btn btn-primary">Thuê</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 @include('layout.footer')
+<script src="{{asset('dist/js/phongJs/rentBook.js')}}"></script>
