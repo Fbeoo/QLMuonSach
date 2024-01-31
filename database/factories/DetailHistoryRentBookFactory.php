dca59e8546a3c4ddc51cee09f511023e11e40608
Phong <hongphongle774@gmail.com>
2024-01-31 17:11:19 +0700

    fix detail history rent book factory

--- database/factories/DetailHistoryRentBookFactory.php
+++ database/factories/DetailHistoryRentBookFactory.php
@@ -19,10 +19,7 @@ class DetailHistoryRentBookFactory extends Factory
     public function definition(): array
     {
         return [
-            'expiration_date' => fake()->dateTimeBetween('2024/3/17','2024/4/17'),
             'quantity' => rand(1,3),
-            'return_date' => fake()->dateTimeBetween('2024/1/17','2024/3/17'),
-            'status' => rand(0,1),
             'book_id' => Book::query()->inRandomOrder()->first()->id,
             'history_rent_book_id' => HistoryRentBook::query()->inRandomOrder()->first()->id
         ];
