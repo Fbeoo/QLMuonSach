73df91228b29089705547ae2e146c2ea9daa5edc
Phong <hongphongle774@gmail.com>
2024-01-31 17:13:45 +0700

    add function to get history rent book for user and request rent book for admin

--- app/Repositories/Eloquent/HistoryRentBookRepository.php
+++ app/Repositories/Eloquent/HistoryRentBookRepository.php
@@ -4,4 +4,23 @@ use App\Repositories\HistoryRentBookRepositoryInterface;
 
 class HistoryRentBookRepository extends BaseRepository implements HistoryRentBookRepositoryInterface {
 
+    public function getHistoryRentBookOfUser($userId)
+    {
+        try {
+            $historyRent = $this->model->where('user_id',$userId)->get();
+            return $historyRent;
+        }catch (\Exception $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    public function getHistoryRentBookForAdmin()
+    {
+        try {
+            $historyRent = $this->model->with('user')->get();
+            return $historyRent;
+        }catch (\Exception $e) {
+            throw new \Exception($e);
+        }
+    }
 }
--- app/Repositories/HistoryRentBookRepositoryInterface.php
+++ app/Repositories/HistoryRentBookRepositoryInterface.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Repositories;
 interface HistoryRentBookRepositoryInterface extends EloquentRepositoryInterface {
+    public function getHistoryRentBookOfUser($userId);
 
+    public function getHistoryRentBookForAdmin();
 }
