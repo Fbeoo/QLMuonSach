48ee5266a3731eaa63b84508d323ddc1a21c947f
Phong <hongphongle774@gmail.com>
2024-01-31 17:16:10 +0700

    add js rent book

---
+++ public/dist/js/phongJs/rentBook.js
@@ -0,0 +1,52 @@
+document.getElementById('addToCart').addEventListener('click',function () {
+    event.preventDefault();
+    var formData = new FormData($('#formAddToCart')[0]);
+    $.ajax({
+        url: 'http://localhost:8000/api/add-to-cart',
+        method: 'POST',
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+        data: formData,
+        error: function(xhr, textStatus, errorThrown) {
+            console.log(xhr.responseText);
+            alert('Lỗi: ' + xhr.responseText);
+        },
+        success: function (response) {
+            console.log(response)
+            if (response.success) {
+                alert('Thêm vào giỏ hàng thành công')
+            }
+            else if (response.error) {
+                alert(response.error);
+            }
+        }
+    });
+});
+
+document.getElementById('rent').addEventListener('click',function () {
+    event.preventDefault();
+
+    var formDataRentBook = new FormData($('#formRentBook')[0]);
+    $.ajax({
+        url: 'http://localhost:8000/api/rent-single-book',
+        method: 'POST',
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+        data: formDataRentBook,
+        error: function(xhr, textStatus, errorThrown) {
+            console.log(xhr.responseText);
+            alert('Lỗi: ' + xhr.responseText);
+        },
+        success: function (response) {
+            console.log(response)
+            if (response.success) {
+                alert('Thuê sách thành công');
+            }
+            else if (response.error) {
+                alert(response.error);
+            }
+        }
+    });
+})
