cb83f5f507b713505a5931825506be308bac9d64
Phong <hongphongle774@gmail.com>
2024-02-01 17:12:06 +0700

    add function to accept, refuse and mark to returned book for request rent book

--- app/Http/Controllers/HistoryRentBookController.php
+++ app/Http/Controllers/HistoryRentBookController.php
@@ -39,7 +39,7 @@ class HistoryRentBookController extends Controller
 
     public function acceptRequestRentBook(Request $request) {
         try {
-            $requestRentBook = $this->historyRentBookRepository->find($request->input('requestRentBookId'));
+            $requestRentBook = $this->historyRentBookRepository->find($request->input('requestId'));
             if (!$requestRentBook) {
                 return response()->json(['error'=>'Không tìm thấy yêu cầu mượn sách']);
             }
@@ -50,4 +50,43 @@ class HistoryRentBookController extends Controller
             throw new \Exception($e);
         }
     }
+
+    public function refuseRequestRentBook(Request $request) {
+        try {
+            $requestRentBook = $this->historyRentBookRepository->find($request->input('requestId'));
+            if (!$requestRentBook) {
+                return response()->json(['error'=>'Không tìm thấy yêu cầu mượn sách']);
+            }
+            $requestRentBook->status = HistoryRentBook::statusRefuse;
+            $this->historyRentBookRepository->update($requestRentBook);
+            return response()->json(['success'=>'Từ chối yêu cầu mượn sách thành công']);
+        }catch (\Exception $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    public function markReturnedBook(Request $request) {
+        try {
+            $requestRentBook = $this->historyRentBookRepository->find($request->input('requestId'));
+            if (!$requestRentBook) {
+                return response()->json(['error'=>'Không tìm thấy yêu cầu mượn sách']);
+            }
+            $requestRentBook->status = HistoryRentBook::statusReturned;
+            $requestRentBook->return_date = now()->format('Y/m/d');
+            $this->historyRentBookRepository->update($requestRentBook);
+            return response()->json(['success'=>'Đánh dấu người mượn trả sách thành công']);
+        }catch (\Exception $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    public function getDetailRequest($requestId) {
+        try {
+            $detailRequest = $this->historyRentBookRepository->getDetailRequestRentBook($requestId);
+            return $detailRequest;
+        }catch (\Exception $e) {
+            return response()->json(['error'=>$e]);
+        }
+    }
+
 }
