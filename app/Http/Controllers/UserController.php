2df3ef71f9eea703c3156cb80c5d681e128a0e0c
Phong <hongphongle774@gmail.com>
2024-02-05 16:21:07 +0700

    add function show all , lock and unlock user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Repositories\UserRepositoryInterface;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use MongoDB\Driver\Session;
 
 class UserController extends Controller
 {
+
+    protected $userRepository;
+
+    /**
+     * @param $userRepository
+     */
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+
     /**
      * @param Request $request
      * @return \Illuminate\Http\RedirectResponse
@@ -17,5 +34,80 @@ class UserController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect()->route('login');
+    }
+
+    public function showAllUser() {
+        try {
+            $users = $this->userRepository->getAllUser();
+            return view('admin.manageUser',['users'=>$users]);
+        }catch (\Exception $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    public function editProfile(Request $request) {
+        try {
+            $validation = Validator::make($request->all(),[
+                'name' => 'required',
+                'dob' => 'required|date_format:d/m/Y',
+                'address' => 'required'
+            ],[
+                'name.required' => 'Tên người dùng không được để trống',
+
+                'dob.required' => 'Ngày sinh không được để trống',
+                'dob.date_format' => 'Ngày sinh không đúng định dạng',
+
+                'address.required' => 'Địa chỉ không được để trống'
+            ]);
+
+            if ($validation->fails()) {
+                return response()->json(['errorValidate'=>$validation->errors()]);
+            }
+
+            $user = $this->userRepository->find($request->input('userId'));
+
+            $user->name = $request->input('name');
+
+            $dob = Carbon::createFromFormat('d/m/Y',$request->input('dob'))->format('Y/m/d');
+            $user->dob = $dob;
+
+            $user->address = $request->input('address');
+
+            $this->userRepository->update($user);
+
+            \session()->put('user',$user);
+
+            return response()->json(['success'=>'Sửa thông tin thành công']);
+        }catch (\Exception $e) {
+            return response()->json(['error'=>$e]);
+        }
+    }
+
+    public function lockUser($userId) {
+        try {
+            $user = $this->userRepository->find($userId);
+
+            $user->status = User::statusLock;
+
+            $this->userRepository->update($user);
+
+            return response()->json(['success'=>'Khóa người dùng thành công']);
+        }catch (\Exception $e) {
+            return response()->json(['error'=>$e]);
+        }
+    }
+
+    public function unlockUser($userId) {
+        try {
+            $user = $this->userRepository->find($userId);
+
+            $user->status = User::statusNormal;
+
+            $this->userRepository->update($user);
+
+            return response()->json(['success'=>'Mở khóa người dùng thành công']);
+        }catch (\Exception $e) {
+            return response()->json(['error'=>$e]);
+        }
     }
 }
