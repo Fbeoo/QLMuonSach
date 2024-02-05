77734815759a0c1120ddd6f663601d881617bc91
Phong <hongphongle774@gmail.com>
2024-02-05 16:19:10 +0700

    fix view confirm rent book

--- resources/views/requestConfirmation.blade.php
+++ resources/views/requestConfirmation.blade.php
@@ -15,16 +15,15 @@
             <!-- /.col -->
             <div class="col-md-6 invoice-col">
                 <address>
-                    <strong>John Doe</strong><br>
-                    Address: 795 Folsom Ave, Suite 600 San Francisco, CA 94107<br>
-                    Phone: (555) 539-1037<br>
-                    Email: john.doe@example.com
+                    <strong>{{session()->get('user')->name}}</strong><br>
+                    Địa chỉ: {{session()->get('user')->address}}<br>
+                    Email: {{session()->get('user')->mail}}
                 </address>
             </div>
             <!-- /.col -->
             <div class="col-md-6 invoice-col">
-                <b>Date rent:</b> 2/22/2014<br>
-                <b>Date expire:</b> 2/22/2014
+                <b>Ngày mượn:</b> {{$dateRent}}<br>
+                <b>Ngày trả:</b> <p id="dateExpire">{{$dateExpire}}</p>
             </div>
             <!-- /.col -->
         </div>
@@ -38,7 +37,6 @@
             <!-- /.col -->
         </div>
         <!-- /.row -->
-
         <div class="row">
             <!-- accepted payments column -->
             <!-- /.col -->
@@ -55,34 +53,90 @@
                             </tr>
                             </thead>
                             <tbody id="cartContent">
-                                <tr class="odd">
-                                    <td style="width: 150px; height: 150px">
-                                        <img style="max-width: 100%; max-height: 100%; padding-left: 25px" src="">
-                                    </td>
-                                    <td style="text-align: center; vertical-align: middle"></td>
-                                    <td style="text-align: center; vertical-align: middle"></td>
-                                    <td style="text-align: center; vertical-align: middle; width: 150px">
+                                @if($typeRent === 'rentSingleBook')
+                                    <input id="bookId" type="hidden" value="{{$book->id}}">
+                                    <input id="quantityRent" type="hidden" value="{{$quantityRent}}">
+                                    <input id="totalPrice" type="hidden" value="{{$totalPrice}}">
+                                    <tr class="odd">
+                                        <td style="width: 150px; height: 150px">
+                                            <img style="max-width: 100%; max-height: 100%; padding-left: 25px" src="http://localhost:8000/storage/{{$book->thumbnail}}">
+                                        </td>
+                                        <td style="text-align: center; vertical-align: middle">{{$book->name}}</td>
+                                        <td style="text-align: center; vertical-align: middle">{{$book->price_rent}}</td>
+                                        <td style="text-align: center; vertical-align: middle; width: 150px">{{$quantityRent}}</td>
+                                        <td style="text-align: center; vertical-align: middle; width: 150px">{{$totalPrice}}</td>
+                                    </tr>
+                                @elseif($typeRent === 'rentMultiBook')
+                                    @foreach($cart->get('bookInCart') as $book)
+                                        <tr class="odd">
+                                            <td style="width: 150px; height: 150px">
+                                                <img style="max-width: 100%; max-height: 100%; padding-left: 25px" src="http://localhost:8000/storage/{{$book['book']->thumbnail}}">
+                                            </td>
+                                            <td style="text-align: center; vertical-align: middle">{{$book['book']->name}}</td>
+                                            <td style="text-align: center; vertical-align: middle">{{$book['book']->price_rent}}</td>
+                                            <td style="text-align: center; vertical-align: middle; width: 150px">{{$book['quantityLine']}}</td>
+                                            <td style="text-align: center; vertical-align: middle; width: 150px">{{$book['linePrice']}}</td>
+                                        </tr>
+                                    @endforeach
+                                @endif
 
-                                    </td>
-                                    <td></td>
-                                </tr>
                             </tbody>
                         </table>
                 </div>
             </div>
             <!-- /.col -->
         </div>
+        @if($typeRent === 'rentSingleBook')
+            <div class="row">
+                <div class="col-6" style="text-align: left">
+
+                </div>
+                <div class="col-6" style="text-align: right">
+                    <div class="row">
+                        <div class="col-10">
+                            Tổng tiền
+                        </div>
+                        <div class="col-2">{{$totalPrice}}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="margin-top: 20px">
+                <div class="col-12">
+                    <a class="btn btn-success float-right" id="rentSingleBook">
+                        Thuê
+                    </a>
+                </div>
+            </div>
+        @elseif($typeRent === 'rentMultiBook')
+            <div class="row">
+                <div class="col-6" style="text-align: left">
+
+                </div>
+                <div class="col-6" style="text-align: right">
+                    <div class="row">
+                        <div class="col-10">
+                            Tổng tiền
+                        </div>
+                        <div class="col-2">
+                            {{$cart->get('totalPrice')}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="margin-top: 20px">
+                <div class="col-12">
+                    <a class="btn btn-success float-right" id="rentMultiBook">
+                        Thuê
+                    </a>
+                </div>
+            </div>
+        @endif
         <!-- /.row -->
 
         <!-- this row will not appear when printing -->
-        <div class="row no-print">
-            <div class="col-12">
-                <button type="button" class="btn btn-success float-right"><i class="far fa-credit-card"></i> Submit
-                    Thuê
-                </button>
-            </div>
-        </div>
+
     </div>
     <!-- /.invoice -->
 </div>
+<script src="{{asset('dist/js/phongJs/confirmRequestRentBook.js')}}"></script>
 @include('layout.footer')
