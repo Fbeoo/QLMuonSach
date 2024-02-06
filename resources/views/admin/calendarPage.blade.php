abea6597ea48f91d1b2525ef11d0bdedb7c585d8
Phong <hongphongle774@gmail.com>
2024-02-06 16:31:47 +0700

    add view calendar page

---
+++ resources/views/admin/calendarPage.blade.php
@@ -0,0 +1,16 @@
+@include('admin.layout.header')
+@include('admin.layout.sidebar')
+<div  style="height: 800px; width: 80%; margin: auto">
+    <div id='calendar' style="max-height: 100%; max-width: 100%"></div>
+</div>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+
+    });
+</script>
+<script src="{{asset('dist/js/phongJs/calendar.js')}}"></script>
+@include('admin.layout.footer')
