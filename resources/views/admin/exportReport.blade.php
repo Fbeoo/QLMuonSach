d05e944bb32abfda405f3af3b464b0d6747506c8
Phong <hongphongle774@gmail.com>
2024-02-20 16:55:49 +0700

    fix view export report

--- resources/views/admin/exportReport.blade.php
+++ resources/views/admin/exportReport.blade.php
@@ -2,26 +2,33 @@
 @include('admin.layout.sidebar')
 <form id="formExportReport">
     @csrf
-<div class="row" style="width: 80%; margin: auto; padding-top: 30px">
-        <div class="col-6">
-            <label>Loại báo cáo</label>
-            <select name="typeReport" id="report" class="form-control custom-select">
-                <option value="" selected>Lựa chọn loại báo cáo cần xuất</option>
-                <option value="bookRent">Sách được mượn</option>
+    <div class="row" style="width: 80%; margin: auto; padding-top: 30px">
+        <div class="col-4">
+            <label>Danh mục báo cáo</label>
+            <select name="categoryReport" id="categoryReport" class="form-control custom-select">
+                <option value="" selected>Lựa chọn danh mục báo cáo</option>
+                <option value="book">Sách</option>
+                <option value="requestRent">Yêu cầu mượn</option>
             </select>
         </div>
-        <div class="col-6" id="time">
+        <div class="col-4">
+            <label>Loại báo cáo</label>
+            <select name="typeReport" id="report" class="form-control custom-select">
+
+            </select>
+        </div>
+        <div class="col-4" id="time">
             <div class="form-group">
                 <label>Thời gian</label>
-                <input class="form-control" type="text" name="dateRangeReport" id="dateRangeReport" value=""/>
+                <input class="form-control" type="text" name="dateRangeReport" id="dateRangeReport" value="" disabled/>
             </div>
         </div>
-</div>
-<div style="width: 10%; margin: 30px auto;">
-        <a id="export" class="btn btn-success">
-            Xuất báo cáo
-        </a>
-</div>
+    </div>
+    <div style="width: 10%; margin: 30px auto;">
+            <a id="export" class="btn btn-success">
+                Xuất báo cáo
+            </a>
+    </div>
 </form>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.2/FileSaver.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
