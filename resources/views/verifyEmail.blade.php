92c39acc02f25141f4eb578fe24f97c83d724e6b
Phong <hongphongle774@gmail.com>
2024-02-27 15:57:36 +0700

    fix url

--- resources/views/verifyEmail.blade.php
+++ resources/views/verifyEmail.blade.php
@@ -1,2 +1,2 @@
 <p>Hello {{$user->name}}</p>
-<p><a href="{{url('/verify/email/'.$user->remember_token)}}">Bấm vào đây</a> để có thể kích hoạt tài khoản</p>
+<p><a href="{{$urlVerifyAccount}}">Bấm vào đây</a> để có thể kích hoạt tài khoản</p>
