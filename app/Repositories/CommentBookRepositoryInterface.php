91684531244403e50c550fd8ccb61f28963ef75e
Phong <hongphongle774@gmail.com>
2024-03-01 11:37:50 +0700

    add function get comment by book id

--- app/Repositories/CommentBookRepositoryInterface.php
+++ app/Repositories/CommentBookRepositoryInterface.php
@@ -1,5 +1,5 @@
 <?php
 namespace App\Repositories;
 interface CommentBookRepositoryInterface extends EloquentRepositoryInterface {
-
+    public function getCommentOfBook($bookId);
 }
--- app/Repositories/Eloquent/CommentBookRepository.php
+++ app/Repositories/Eloquent/CommentBookRepository.php
@@ -4,4 +4,13 @@ use App\Repositories\CommentBookRepositoryInterface;
 
 class CommentBookRepository extends BaseRepository implements CommentBookRepositoryInterface {
 
+    public function getCommentOfBook($bookId)
+    {
+        try {
+            $comments = $this->model->where('book_id',$bookId)->with('user')->get();
+            return $comments;
+        }catch (\Exception $e) {
+            throw new \Exception($e);
+        }
+    }
 }
