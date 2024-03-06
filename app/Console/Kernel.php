f4c7f0b71d045918cba742b585d7ab6ab918ec52
Phong <hongphongle774@gmail.com>
2024-03-06 14:59:29 +0700

    add command to run schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\SendNotification;
+use App\Repositories\UserRepositoryInterface;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +14,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+
         // $schedule->command('inspire')->hourly();
+        $schedule->command('sendNotification:cron')->everyMinute();
     }
 
     /**
